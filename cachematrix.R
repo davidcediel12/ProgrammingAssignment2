05d43b4f6e1a6309f8293c1ef7f42be180a3b178
David Cediel <esdago18@gmail.com>
2020-09-01 15:22:19 -0500

    Add the content of 'makeCacheMatrix' and 'cacheSolve', and put an example

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL
+      
+      get <- function() x 
+      set <- function(m) x <<- m
+      
+      getInverse <- function() inverse
+      setInverse <- function(i) inverse <<- i
+      list(set = set, get = get, 
+           getInverse = getInverse, 
+           setInverse = setInverse)
 }
 
 
@@ -12,4 +21,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inver <- x$getInverse()
+      if(!is.null(inver))
+            return(inver)
+      
+      x$setInverse(solve(x$get(), ...))
+      invisible(x$getInverse())
 }
+
+m <- makeCacheMatrix(matrix(rnorm(250000), nrow = 500, ncol = 500))
+
+tini1 <- Sys.time()
+cacheSolve(m)
+tfin1 <- Sys.time()
+cat("Time without cache", tfin1-tini1, "secs\n")
+
+tini2 <- Sys.time()
+cacheSolve(m)
+tfin2 <- Sys.time()
+cat("Time with cache", tfin2-tini2, "secs")
